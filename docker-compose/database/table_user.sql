ca0d1e09d3aea53d90bba1e14081e600c9ff2461
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2024-07-11 15:00:55 +0900

    Update table_user.sql

--- docker-compose/database/table_user.sql
+++ docker-compose/database/table_user.sql
@@ -3,10 +3,12 @@ CREATE TABLE IF NOT EXISTS users (
 	uuid text primary key,
 	name text not null,
 	email name not null,
+	roles int not null,
 	created_at int default EXTRACT(EPOCH FROM CURRENT_TIMESTAMP),
 	modfied_at int default EXTRACT(EPOCH FROM CURRENT_TIMESTAMP),
 	deleted_at int default EXTRACT(EPOCH FROM CURRENT_TIMESTAMP),
 	online bool default true,
 	deleted bool default false,
-	nodeid serial not null
+	nodeid serial not null,
+	FOREIGN KEY (roles) REFERENCES roles (uuid)
 )
